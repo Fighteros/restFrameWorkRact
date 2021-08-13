589738a066f06b7a8947dd63e16c21d6e6d77545
fighteros <fighteros121212@gmail.com>
2021-08-13 03:31:36 +0200

    finished backend

---
+++ backend/todos/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+
+from todos.models import Todo
+from todos.serializers import TodoSerializer
+
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
